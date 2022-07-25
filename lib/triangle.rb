9ae04ace0068aa1970dbf38859f9ccc176e3130c
NickeaBennett <nickeabennett@gmail.com>
2022-07-25 13:25:02 -0400

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
   # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    [a, b, c].permutation do |sides|
+      raise TriangleError unless sides[0] + sides[1] > sides[2]
+    end
+    case [a,b,c].uniq.size
+      when 3; :scalene
+      when 2; :isosceles
+      when 1; :equilateral
+    end
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
